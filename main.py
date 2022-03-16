de9e5c923afceded0d34d188eead47d24f6bf097
danielvigild <65343336+danielvigild@users.noreply.github.com>
2022-03-16 12:30:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
-# Run this app with `python app.py` and
-# visit http://127.0.0.1:8050/ in your web browser.
-
-from dash import Dash, html, dcc
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
 import plotly.express as px
 import pandas as pd
 
-app = Dash(__name__)
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 # assume you have a "long-form" data frame
 # see https://plotly.com/python/px-arguments/ for more options
-df = pd.read_csv('nba_data.csv')
+df = pd.read_csv('nba_data.csv',delimiter=";")
 
-fig = px.bar(df, x="Player", y="Points", color="Team", barmode="group")
+fig = px.bar(df, x='Player', y="Points", color="Team")
 
 app.layout = html.Div(children=[
     html.H1(children='Hello Dash'),
