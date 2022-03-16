c6741a806085acd7fde83c849ae399443ffe9553
danielvigild <65343336+danielvigild@users.noreply.github.com>
2022-03-16 13:54:57 +0100

    Update main.py

--- main.py
+++ main.py
@@ -28,4 +28,4 @@ app.layout = html.Div(children=[
 ])
 
 if __name__ == '__main__':
-    app.server(host='0.0.0.0', port=8080, debug=True)
+    app.run_server(host='0.0.0.0', port=8080, debug=True, use_reloader=False)
